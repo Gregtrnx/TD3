b92933ed9c64353b88d77ddbbe64e6f4c3123a9f
Gregtrnx <146743037+Gregtrnx@users.noreply.github.com>
2024-09-11 09:21:45 +0200

    Update index-script.js

--- js/index-script.js
+++ js/index-script.js
@@ -3,4 +3,7 @@ window.addEventListener('click', chat);
 function chat(event) {
     let posiX = event.pageX || event.changedTouches[0].pageX;
     console.log(posiX);
+    document.querySelector('.sousTitre').innerHTML='<span>' + posiX + '</span>';
 }   
+
+ 
